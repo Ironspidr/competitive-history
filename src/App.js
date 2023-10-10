7797bee53b43fea5372e0d949e2d29571f2b2c17
Ironspidr <janatanielroshan@gmail.com>
2023-10-10 16:49:34 -0700

    form experiment

--- src/App.js
+++ src/App.js
@@ -1,82 +1,35 @@
 import "./App.css";
-import { useState } from "react";
 
 const SearchForm = () => {
-  const [state, setState] = useState({
-    name: "",
-    event: "",
-    year: "",
-    conference: "",
-  });
-
-  const handleChange = (e) => {
-    setState({ ...state, [e.target.id]: e.target.value });
-  };
-  const yearsList = ["2023", "2022", "2021", "2020", "2019", "2018", "2017"];
-  const conferences = ["ISLC", "SLC", "NLC"];
-  const yearsOptions = yearsList.map((year, key) => (
-    <option value={year} key={key} id="year" onchange>
-      {year}
-    </option>
-  ));
-  const conferenceOptions = conferences.map((conference, key) => (
-    <option
-      value={conference}
-      id="conference"
-      key={key}
-      onChange={handleChange}
-    >
-      {conference}
-    </option>
-  ));
   return (
     <div>
       <h1>Competitive History</h1>
       <form>
-        <label>
-          Name:
-          <input
-            type="text"
-            id="name"
-            value={state.name}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Event:
-          <input
-            type="text"
-            id="event"
-            value={state.event}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Year:
-          <select name="year" value={state.year} onChange={handleChange}>
-            <option value="" disabled>
-              -- Select a Year --
-            </option>
-            {yearsOptions}
+        <div>
+          <label>Name</label>
+          <br></br>
+          <input type="text" name="name" />
+        </div>
+        <div>
+          <label>Event</label>
+          <br></br>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Year</label>
+          <br></br>
+          <select>
+            <option value="2023">2023</option>
+            <option value="2022">2022</option>
+            <option value="2021">2021</option>
+            <option value="2020">2020</option>
+            <option value="2019">2019</option>
+            <option value="2018">2018</option>
+            <option value="2017">2017</option>
           </select>
-        </label>
-        <label>
-          Conference:
-          <select
-            name="conference"
-            value={state.conference}
-            onchange={handleChange}
-          >
-            <option value="" disabled>
-              -- Select a Conference --
-            </option>
-            {conferenceOptions}
-          </select>
-        </label>
+        </div>
+        <button type="submit">Submit</button>
       </form>
-      <h1>
-        {state.name} {state.event} {state.year} {state.conference}
-      </h1>
     </div>
   );
 };
